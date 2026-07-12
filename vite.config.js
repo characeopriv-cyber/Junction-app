d0df40c4e4f411208e209206c3d3a2f796b3c90e
characeopriv-cyber <characeopriv@gmail.com>
2026-07-12 12:43:49 +0400

    "fix:Remove 'react-router-dom' from vendor chunks

--- vite.config.js
+++ vite.config.js
@@ -7,8 +7,9 @@ export default defineConfig({
     rollupOptions: {
       output: {
         manualChunks: {
-          vendor: ['react', 'react-dom', 'react-router-dom'],
+          vendor: ['react', 'react-dom'],
           charts: ['recharts'],
+          icons: ['lucide-react'],
         },
       },
     },
